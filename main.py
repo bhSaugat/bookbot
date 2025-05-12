84fc2fe91e9c0491e68208fdfc475bcb8bdd7a83
Saugat <saugatwrk@gmail.com>
2025-05-12 02:37:47 +0000

    Removed cached py file to untrack and added both function on main

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import get_book_text
+from stats import get_book_text, occurences
 def main():
-    get_book_text()
+  get_book_text() 
+  occurences()
 main()
--- stats.py
+++ stats.py
@@ -1,7 +1,18 @@
 def get_book_text():
     with open("./books/frankenstein.txt") as f:
         contents = f.read()
-        print(type(contents))
         splitted = contents.split()
         length = len(splitted)
         print(f"{length} words found in the document")
+
+def occurences():
+    count = {}
+    with open("./books/frankenstein.txt") as f:
+        contents = f.read()
+        lowered_contents = contents.lower()
+        for char in lowered_contents:
+            if char in count:
+                count[char] += 1
+            else:
+                count[char] = 1
+        print(count)
