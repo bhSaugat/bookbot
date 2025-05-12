f357a5843c117a56320e9d8b9844b8c532c98af7
Saugat <saugatwrk@gmail.com>
2025-05-12 02:14:57 +0000

    First Program to analyze text

---
+++ main.py
@@ -0,0 +1,10 @@
+def get_book_text():
+    with open("./books/frankenstein.txt") as f:
+        contents = f.read()
+        print(type(contents))
+        splitted = contents.split()
+        length = len(splitted)
+        print(f"{length} words found in the document")
+def main():
+    get_book_text()
+main()
