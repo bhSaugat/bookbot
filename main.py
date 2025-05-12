2b307d575d53344da2ea7f86294a1b59a1d29d8b
Saugat <saugatwrk@gmail.com>
2025-05-12 04:36:29 +0000

    Added sys argv so filepath needn't be hardcoded

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-from stats import get_book_text, occurences
+from stats import get_book_text, occurences, listed_chars, sort_on
+import sys
 def main():
-  get_book_text() 
-  occurences()
+  if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+  else:
+       book_path = sys.argv[1]
+  char_list = listed_chars(occurences(book_path))
+  char_list.sort(reverse=True, key=sort_on)
+  print(f"============ BOOKBOT ============\nAnalyzing book found at {book_path}...\n----------- Word Count ----------\nFound {get_book_text(book_path)} total words\n--------- Character Count -------")
+  for char in char_list:
+    print(f"{char["char"]}: {char["num"]}")
+    
+
 main()
--- stats.py
+++ stats.py
@@ -1,13 +1,13 @@
-def get_book_text():
-    with open("./books/frankenstein.txt") as f:
+def get_book_text(text_path):
+    with open(text_path) as f:
         contents = f.read()
         splitted = contents.split()
         length = len(splitted)
-        print(f"{length} words found in the document")
-
-def occurences():
+        """ print(f"{length} words found in the document") """
+        return length
+def occurences(text_path):
     count = {}
-    with open("./books/frankenstein.txt") as f:
+    with open(text_path) as f:
         contents = f.read()
         lowered_contents = contents.lower()
         for char in lowered_contents:
@@ -15,4 +15,27 @@ def occurences():
                 count[char] += 1
             else:
                 count[char] = 1
-        print(count)
+        """ print(count) """
+    return count
+
+""" def sorted_chars(dict):
+    collected_chars = []
+    tmp_dict = {}
+    for key, value in dict.items():
+        if key.isalpha():
+            tmp_dict = {"char": key, "num": value}
+            collected_chars.append(tmp_dict)
+    sorted_people = sorted(collected_chars, key=lambda x: x['char'])
+    print(sorted_people) """
+
+def listed_chars(dict):
+    collected_chars = []
+    tmp_dict = {}
+    for key, value in dict.items():
+        if key.isalpha():
+            tmp_dict = {"char": key, "num": value}
+            collected_chars.append(tmp_dict)
+    return collected_chars
+
+def sort_on(dict):
+    return dict["num"]
